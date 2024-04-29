9d76a9c64e99b7fecd8dd76b2789c1acd0360e6b
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2024-04-29 23:07:39 +0300

    finished task

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,20 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map((
+      movie
+    ) => {
+      return (
+        <MovieCard
+          // eslint-disable-next-line
+          key={movie.imdbId}
+          movie={movie}
+        />
+      );
+    })}
+  </div>
+);
